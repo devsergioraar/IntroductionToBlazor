39277d87f23a895e8fb0c842df66399d5fdce1d5
devsergioraar <sramirezartavia@gmail.com>
2024-07-17 15:30:06 -0600

    Introduction to Blazor

---
+++ Models/Server.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerManagement.Models
+{
+	public class Server 
+	{
+		public Server() { 
+			Random random = new Random();
+			int randomNumber = random.Next(0,2);
+
+			this.IsOnline = randomNumber == 0 ? false: true ;	
+		}
+        public int ServerId { get; set; }
+		public bool IsOnline { get; set; }
+		[Required(ErrorMessage ="THIS IS A CUSMTOM ERROR")]
+		public string? Name { get; set;}
+		[Required]
+		 
+		public string? City { get; set;}
+	
+    }
+}
